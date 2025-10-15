d6a23d6bf36b05271e538a01a8f47dfe3b9bc6ae
ErikaLaine <erika.laine@student.hamk.fi>
2025-10-15 19:44:59 +0300

    Delete onko mustetta jäljellä.py

--- onko mustetta jäljellä.py
+++
@@ -1,6 +0,0 @@
-mustetta_jaljella = input("Onko tulostimessa mustetta? (kyllä/ei): ").strip().lower()
-
-if mustetta_jaljella == "kyllä":
-    print("Tulostus onnistui!")
-else:
-    print("Tulostus epäonnistui: Ei mustetta.")
