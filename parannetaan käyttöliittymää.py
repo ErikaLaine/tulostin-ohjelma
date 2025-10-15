db46e5c455ff4bb1db0112463ea2e58c293225af
ErikaLaine <erika.laine@student.hamk.fi>
2025-10-15 19:45:27 +0300

    Delete parannetaan käyttöliittymää.py

--- parannetaan käyttöliittymää.py
+++
@@ -1,8 +0,0 @@
-tiedosto = input("Anna tulostettavan tiedoston nimi: ")
-
-mustetta_jaljella = input("Onko tulostimessa mustetta? (kyllä/ei): ").strip().lower()
-
-if mustetta_jaljella == "kyllä":
-    print(f"Tulostetaan tiedosto: {tiedosto}")
-else:
-    print("Tulostus epäonnistui: Ei mustetta.")
