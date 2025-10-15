09a06cbc6496bc18febd5f5877c323f6e03c4613
ErikaLaine <erika.laine@student.hamk.fi>
2025-10-15 19:45:36 +0300

    Delete tulostin-ohjelma.py

--- tulostin-ohjelma.py
+++
@@ -1,8 +0,0 @@
-teksti = input("Mitä haluat tulostaa? ")
-
-try:
-    maara = int(input("Kuinka monta kertaa? "))
-    for i in range(maara):
-        print(teksti)
-except ValueError:
-    print("Virhe: kirjoita numerona toistojen määrä.")
